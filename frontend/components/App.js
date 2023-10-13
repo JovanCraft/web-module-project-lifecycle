5cc40d5dc567ad17fa982de29617372edbe239f8
jovan <jovancraft@gmail.com>
2023-10-13 05:52:57 -0500

    testing commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+  fetchTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      
+    })
+    .catch()
+  }
+  componentDidMount() {
+
+  }
   render() {
     return null
   }
